65ac2d026011e430b78bde4df6a3e7bcdb17a9b2
Chikara Orval <chikara.orval@eur.nl>
2025-12-04 13:02:13 +0100

    day3

--- day3/main.js
+++ day3/main.js
@@ -15,4 +15,21 @@ const part1 = () => {
   return input.reduce((sum, line) => sum + maxJoltage(line), 0);
 };
 
-console.log(part1());
+const part2 = () => {
+  const maxJoltage = (line, count, start = 0) => {
+    if (count === 0) return '';
+    const endPos = line.length - count;
+    let maxDigit = '0', maxIdx = start;
+    for (let i = start; i <= endPos; i++) {
+      if (line[i] > maxDigit) {
+        maxDigit = line[i];
+        maxIdx = i;
+      }
+    }
+    return maxDigit + maxJoltage(line, count - 1, maxIdx + 1);
+  };
+
+  return input.reduce((sum, line) => sum + Number(maxJoltage(line, 12)), 0);
+};
+
+console.log(`${part1()} ${part2()}`);
