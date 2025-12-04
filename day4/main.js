c0a576aacdea6413a2ab646b5fa929c6b9a2c9eb
Chikara Orval <chikara.orval@eur.nl>
2025-12-04 14:24:25 +0100

    day4part2

--- day4/main.js
+++ day4/main.js
@@ -17,4 +17,28 @@ const part1 = () => {
   return count;
 };
 
-console.log(part1());
+const part2 = () => {
+  const grid = input.map(row => [...row]);
+  let count = 0;
+
+  while (true) {
+    const toRemove = [];
+    for (let r = 0; r < grid.length; r++) {
+      for (let c = 0; c < grid[r].length; c++) {
+        if (grid[r][c] !== '@') continue;
+        const neighbors = [
+          grid[r-1]?.[c-1], grid[r-1]?.[c], grid[r-1]?.[c+1],
+          grid[r]?.[c-1],                   grid[r]?.[c+1],
+          grid[r+1]?.[c-1], grid[r+1]?.[c], grid[r+1]?.[c+1]
+        ].filter(x => x === '@').length;
+        if (neighbors < 4) toRemove.push([r, c]);
+      }
+    }
+    if (toRemove.length === 0) break;
+    for (const [r, c] of toRemove) grid[r][c] = '.';
+    count += toRemove.length;
+  }
+  return count;
+};
+
+console.log(`${part1()} ${part2()}`);
